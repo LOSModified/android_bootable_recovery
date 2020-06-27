958776bf65af39653d4181de9e5f4cdfa88215fd
Dyneteve <dyneteve@pixelexperience.org>
2020-06-27 19:36:32 +0200

    recovery: Use ro.build.product to detect the device codename
    * ro.product.device may be changed to fake props for NGA
    
    Change-Id: If0f713ca46ce645a69b1c6edc4bf1d34dc20e74e

--- fastboot/fastboot.cpp
+++ fastboot/fastboot.cpp
@@ -42,7 +42,7 @@ void FillDefaultFastbootLines(std::vector<std::string>& title_lines) {
   std::string baseband_version = android::base::GetProperty("ro.build.expect.baseband", "");
   std::string hw_version = android::base::GetProperty(
       "ro.boot.hardware.revision", android::base::GetProperty("ro.revision", ""));
-  title_lines.push_back("Product name - " + android::base::GetProperty("ro.product.device", ""));
+  title_lines.push_back("Product name - " + android::base::GetProperty("ro.build.product", ""));
   if (!android::base::EqualsIgnoreCase(bootloader_version, "unknown")) {
     title_lines.push_back("Bootloader version - " + bootloader_version);
   }
